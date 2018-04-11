0e0d3226c6a7679edc7b172120c054f87802420f
Alexander Surkov <surkov.alexander@gmail.com>
2018-04-10 22:33:24 -0400

    add test itself

--- kotlin/sample/src/main/kotlin/Main.kt
+++ kotlin/sample/src/main/kotlin/Main.kt
@@ -3,8 +3,15 @@
 
 package sample
 
+import com.github.kittinunf.fuel.core.Response
+import java.net.URL
 import vacuumanalytics.LogEvent
 
 fun main(argv : Array<String>) {
-  println("HEYHEYEYEHHE")
+  val fakeResponse = Response(
+    url = URL("http://0.0.0.0"),
+    headers = mapOf("Set-Cookie" to listOf("sessionid=kotlintst"))
+  )
+  val logger = LogEvent("http://0.0.0.0:8080", fakeResponse, "sessionid")
+  logger.error("B'eba-%pBeba")
 }
